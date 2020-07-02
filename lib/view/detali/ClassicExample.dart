8f93ba27c7acd38971204d2f07f7538ee5455c16
xnxgt <15538105686@163.com>
2020-07-02 18:36:27 +0800

    t

---
+++ lib/view/detali/ClassicExample.dart
@@ -0,0 +1,21 @@
+
+import 'package:flutter/material.dart';
+
+class ClassicExample extends StatelessWidget{
+
+  var data;
+
+  ClassicExample(this.data);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Column(
+        children: [
+
+        ],
+      ),
+    );
+  }
+
+}
